b9a95b008dee71e7f1999185e34a483adf6b9fd3
Tatsiana Yasiuchenia <70094257+tasergeevna@users.noreply.github.com>
2021-10-03 18:58:48 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector("#board");
+const colors = ["#f020e8", "#b917f0", "#8117f0", "#5217f0", "#cefa17", "#8ffa16", "#21fa16", "#00f0e5"];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.addEventListener("mouseover", setColor);
+    square.addEventListener("mouseleave", removeColor);
+
+    board.append(square);
+}
+
+function setColor(event) {
+    const element = event.target;
+    const color = getRandomColor();
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(event) {
+    const element = event.target;
+    element.style.backgroundColor = "#1d1d1d";
+    element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
